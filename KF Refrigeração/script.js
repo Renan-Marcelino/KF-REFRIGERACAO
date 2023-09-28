c2f8b6a63baabb5741ea4599d7372a62db5eeef8
Renan Marcelino Da Silva Souza <79055100+Renan-Marcelino@users.noreply.github.com>
2023-09-28 10:12:26 -0300

    Add files via upload

--- KF Refrigeração/script.js
+++ KF Refrigeração/script.js
@@ -1,6 +1,6 @@
 const menu = document.querySelector('.menu');
 const NavMenu = document.querySelector('.nav-menu');
-
+ 
 menu.addEventListener('click', () => {
     menu.classList.toggle('ativo');
     NavMenu.classList.toggle('ativo');
